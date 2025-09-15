7f191b59cd82f4bdae80713c1f14c0556f67f485
DavidStrikaitis <davidstrik282@gmail.com>
2025-09-15 12:17:10 +0100

    fixed failing test, changed logic to use map

--- src/lib/filters/cubeFilter.ts
+++ src/lib/filters/cubeFilter.ts
@@ -1,8 +1,4 @@
 export function cubeFilter(x: number[]) : number[]
 {
-    let out: number[] = [];
-    x.forEach(num => {
-        out.fill(num * num * num);
-    });
-    return out;
+    return x.map(num => num * num * num);
 }
