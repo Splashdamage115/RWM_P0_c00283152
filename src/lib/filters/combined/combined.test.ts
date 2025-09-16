2a0e4605274e014c629f00b5eeb895862df0b816
DavidStrikaitis <davidstrik282@gmail.com>
2025-09-16 15:11:37 +0100

    working on combining files

---
+++ src/lib/filters/combined/combined.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import { combinedFilter } from './combined';
+
+describe("combined filter", () => {
+    it("Placeholder", () => {
+        expect(true);
+    })
+});
---
+++ src/lib/filters/combined/combined.ts
@@ -0,0 +1,7 @@
+import { cubeFilter as myCube } from '$lib/filters/cubeFilter';
+import { cubes as peerCubes } from 'peer-filter-c00278711';
+
+export function combinedFilter() 
+{
+
+}
